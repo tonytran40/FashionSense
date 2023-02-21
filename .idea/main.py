4d5d5a66457a9f28da22454277da3ea3d14e21b8
Tony <tuk92144@temple.edu>
2023-02-21 18:31:18 -0500

    Update main.py
    added more

--- .idea/main.py
+++ .idea/main.py
@@ -2,10 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-prices = input("What product do you want to look for? ")
-url = f"https://www2.hm.com/en_us/men/products/{prices}.html"
+products = input("What product do you want to look for? ")
+url = f"https://www.uniqlo.com/us/en/search?q={products}"
 page = requests.get(url).text
 doc = BeautifulSoup(page, "html.parser")
 
+page_text=doc.find(class_="list-tool-pagination-text").strong
+pages = int(str(page_text).split("/")[-2].split(">")[-1][-1])
 
-print(url)
+for page in range(1,pages+1):
+    url = f"https://www.uniqlo.com/us/en/search?q={products}"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
+    div = doc.find(class_="fr-ec-product-collection fr-ec-product-collection--type-grid")
+    
+    items = div.find_all(text=re.compile(products))
+    for item in items:
+        print(item)
+    
